0e76e6c03b31b9a83005495f4878c172d8a86627
Samuel Phillips <sam@samuelphillips.net>
2015-11-12 18:37:33 -0500

    Another site

--- hdstuck.user.js
+++ hdstuck.user.js
@@ -8,6 +8,8 @@
 // @include http://www.miniclip.com/games/*/*/*
 // @include https://www.miniclip.com/games/*/*/*
 // @include http://www.engineering.com/GamesPuzzles/Motherload/tabid/4708/Default.aspx
+// @include http://www.kongregate.com/games/*/*
+// @include https://www.kongregate.com/games/*/*
 // @version     1
 // @grant       none
 // ==/UserScript==
