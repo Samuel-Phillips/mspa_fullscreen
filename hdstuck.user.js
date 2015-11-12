d8ef85cc3fefffd302e3b8e5dae5f79b4b109a81
Samuel Phillips <sam@samuelphillips.net>
2015-11-12 18:09:43 -0500

    Added another site

--- hdstuck.user.js
+++ hdstuck.user.js
@@ -7,6 +7,7 @@
 // @include https://www.newgrounds.com/portal/view/*
 // @include http://www.miniclip.com/games/*/*/*
 // @include https://www.miniclip.com/games/*/*/*
+// @include http://www.engineering.com/GamesPuzzles/Motherload/tabid/4708/Default.aspx
 // @version     1
 // @grant       none
 // ==/UserScript==
